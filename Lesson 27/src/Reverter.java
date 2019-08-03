3f1a5b292eebbc7b26cf10fbda890eaef4765791
Nikolay Gorbunov <krot19831983@mail.ru>
2019-08-03 17:45:39 +0300

    Nikolay Gorbunov. Reverted file.

--- Lesson 27/src/Reverter.java
+++ Lesson 27/src/Reverter.java
@@ -1,21 +1,18 @@
 import java.io.*;
 
+/**
+@author
+Nikolay Gorbunov
+version 0.1
+App for reverting chars in txt files on Windows Systems
+*/
+
+
 public class Reverter {
 
 	public static void main(String[] args) {
-		String  filePath= "C:\\test.txt";
-//		DataReciever data = new DataReciever();
-//		data.setFileName(filePath);
-//		StringBuilder source = data.dataToChange(filePath);
-//		System.out.println(new String(source));
-//		System.out.println();
-//		data.dataToChange(filePath);
-//		StringBuilder reverted = new StringBuilder("");
-//		for(int i = source.length(); i > 0; i--) {
-//			reverted.append(source.charAt(i-1));
-//		}
-//		System.out.println(new String(source));
-		System.out.print(new String((new DataReciever("C:\\test.txt").dataToChange())));
+//		System.out.print(new String((new DataReciever("C:\\test.txt").dataToChange())));
+		new DataReciever("C:\\test.txt").fileReverted();
 
 	}
 
@@ -24,80 +21,58 @@ public class Reverter {
 class DataReciever {
 	String fileName;
 	int count;
-//	void setFileName(String fileName){
-//		this.fileName = fileName;
-//	}
-	DataReciever(String fileName){
+
+	/**
+	 * constructor of the class
+	 * @param fileName isa path to the file need to be reverted
+	 */
+	DataReciever(String fileName) {
 		this.fileName = fileName;
 	}
+
 	StringBuilder data = new StringBuilder("");
 	StringBuilder result = new StringBuilder("");
-	
-	StringBuilder dataToChange(){
+
+/**
+ * 
+ * @return reverted StringBuilder object received from class constructor
+ */
+	StringBuilder dataToChange() {
 		try (BufferedReader br = new BufferedReader(new FileReader(new File(fileName)))) {
 			String line;
 			while ((line = br.readLine()) != null) {
-				data.append(line).append(System.lineSeparator());
+//				data.append(line).append(System.lineSeparator()); ??? Почему-то работает неправильно
+				data.append(line).append('\n');
 			}
 			data.deleteCharAt(data.length() - 1);
-			System.out.print(new String(data));
 			br.close();
-		
+
 		} catch (FileNotFoundException e1) {
 			System.out.println("File not found");
 		} catch (NullPointerException e) {
 			System.out.println("Не введено имя файла");
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Файл занят.");
 		}
-		
-		for(int i = data.length(); i > 0; i--) {
-			result.append(data.charAt(i-1));
+
+		for (int i = data.length(); i > 0; i--) {
+			this.result.append(data.charAt(i - 1));
 		}
-		
-		return result;
+
+		return this.result;
 
 	}
-	
-}
-
-class DataWriter{
-	String fileName;
-	int count;
-	DataWriter(String fileName, int count){
-		this.fileName = fileName;
-		this.count = count;
-	}
-	
-    File file = new File(fileName);
-    FileOutputStream fileOutputStream = new FileOutputStream(file);
-
-    String greetings = new String((new DataReciever("C:\\test.txt").dataToChange()));
-    
-    fileOutputStream.write(greetings.getBytes());
-
-//	fileOutputStream.write(greetings.getBytes());
-
-//	fileOutputStream.close();
-    
-	/*
-	try (BufferedWriter wr = new BufferedWriter(new FileWriter(new File(fileName)))) {
-		String line;
-		while ((line = wr.readLine()) != null) {
-			data.append(line).append(System.lineSeparator());
+/**
+ * Void method responsible for writing data to file
+ */
+	void fileReverted() {
+		dataToChange();
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File("test1.txt")))) {
+			String out = new String(result);
+			System.out.println(out);
+			bw.write(new String(out));
+		} catch (IOException e) {
+			System.out.println("Не могу прочитать файл " + "test1.txt");
 		}
-		data.deleteCharAt(data.length() - 1);
-		System.out.print(new String(data));
-		br.close();
-	
-	} catch (FileNotFoundException e1) {
-		System.out.println("File not found");
-	} catch (NullPointerException e) {
-		System.out.println("Не введено имя файла");
 	}
-	catch (IOException e) {
-		System.out.println("Файл занят.");
-	}
-	*/
 }
